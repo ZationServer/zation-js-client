7ec79ce0afe4a10b574d9ca50d45481b27ae7ccf
LucaCode <lucalstyle@gmail.com>
2018-04-05 04:02:38 +0200

    up

---
+++ index.js
@@ -0,0 +1,14 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+©Copyright by Luca Scaringella
+ */
+
+//Api Classes
+module.exports.Bag = require('./system/api/Bag');
+module.exports.Controller = require('./system/api/Controller');
+module.exports.Result = require('./system/api/Result');
+module.exports.TaskError = require('./system/api/TaskError');
+module.exports.TaskErrorBag = require('./system/api/TaskErrorBag');
+
+module.exports.ErrorType = require('./system/helper/constante/errorTypes');
