c830e00432321d821ecb408b5b333f27b90b6c73
Luca Scaringella <lucalstyle@gmail.com>
2019-08-16 15:46:33 +0200

    Added DataBox definitions.

---
+++ src/lib/main/dataBox/dbDefinitions.ts
@@ -0,0 +1,294 @@
+/*
+Author: Luca Scaringella
+GitHub: LucaCode
+Copyright(c) Luca Scaringella
+ */
+
+export interface DataBoxConnectReq {
+    /**
+     * dataBox (name)
+     */
+    d : string,
+    /**
+     * id
+     */
+    i ?: string,
+    /**
+     * apiLevel
+     */
+    al ?: number,
+    /**
+     * token
+     */
+    t ?: string
+    /**
+     * initInput
+     */
+    ii ?: any
+}
+
+export interface DataBoxConnectRes {
+    /**
+     * Last cud id
+     */
+    ci : string,
+    /**
+     * Used the token
+     */
+    ut : boolean,
+    /**
+     * Input key
+     */
+    i : string,
+    /**
+     * Output key
+     */
+    o : string,
+    /**
+     * Parallel fetching
+     */
+    pf : boolean
+}
+
+export const DATA_BOX_START_INDICATOR = '>D';
+
+/**
+ * The package that the client can send to the server to invoke an action.
+ */
+export interface DbClientInputPackage {
+    /**
+     * Action
+     */
+    a : DbClientInputAction,
+    /**
+     * Session Target
+     */
+    t ?: DBClientInputSessionTarget
+}
+
+/**
+ * The package that the client can send to the server to fetch data.
+ */
+export interface DbClientInputFetchPackage extends DbClientInputPackage{
+    a : DbClientInputAction.fetchData,
+    /**
+     * input
+     */
+    i : any
+}
+
+export interface DbClientInputFetchResponse {
+    /**
+     * counter
+     */
+    c : number,
+    /**
+     * token
+     */
+    t : string,
+    /**
+     * data
+     */
+    d : any
+}
+
+/**
+ * Events that a client can receive from the server.
+ */
+export enum DbClientOutputEvent {
+    cud,
+    close,
+    reload,
+    kickOut
+}
+
+/**
+ * Packages that the server can send to the clients.
+ */
+export interface DbClientOutputPackage {
+    /**
+     * action
+     */
+    a : DbClientOutputEvent,
+    /**
+     * data
+     */
+    d ?: any,
+    /**
+     * code
+     */
+    c ?: number | string,
+}
+
+export enum CudType {
+    insert,
+    update,
+    delete
+}
+
+/**
+ * A cud action.
+ */
+export interface CudAction {
+    /**
+     * type
+     */
+    t : CudType,
+    /**
+     * keyPath
+     */
+    k : string[],
+    /**
+     * value
+     */
+    v ?: any;
+    /**
+     * code
+     */
+    c ?: any;
+    /**
+     * data
+     */
+    d ?: any;
+    /**
+     * ifContains
+     */
+    i ?: string;
+}
+
+/**
+ * A full-defined cud package that the server can send to the clients.
+ */
+export interface CudPackage extends PreCudPackage{
+    /**
+     * timestamp
+     */
+    t : number,
+}
+
+/**
+ * A pre-defined cud package.
+ */
+export interface PreCudPackage {
+    /**
+     * cudId
+     */
+    ci : string,
+    /**
+     * timestamp
+     */
+    t ?: number,
+    /**
+     * actions
+     */
+    a : CudAction[]
+}
+
+/**
+ * Cud package that the server can send to the clients.
+ * In case of an insert, update, or delete of data.
+ */
+export interface DbClientOutputCudPackage extends DbClientOutputPackage{
+    /**
+     * action
+     */
+    a : DbClientOutputEvent.cud,
+    /**
+     * data
+     */
+    d : CudPackage
+}
+
+/**
+ * Reload package that the server can send to the clients.
+ */
+export interface DbClientOutputReloadPackage extends DbClientOutputPackage{
+    /**
+     * action
+     */
+    a : DbClientOutputEvent.reload,
+    /**
+     * data
+     */
+    d ?: any
+}
+
+/**
+ * Close package that the server can send to the clients.
+ */
+export interface DbClientOutputClosePackage extends DbClientOutputPackage{
+    /**
+     * action
+     */
+    a : DbClientOutputEvent.close,
+    /**
+     * data
+     */
+    d ?: any
+}
+
+/**
+ * Kick out package that the server can send to the clients.
+ */
+export interface DbClientOutputKickOutPackage extends DbClientOutputPackage{
+    /**
+     * action
+     */
+    a : DbClientOutputEvent.kickOut,
+    /**
+     * data
+     */
+    d ?: any
+}
+
+/**
+ * The target session that the server should use to process.
+ */
+export enum DBClientInputSessionTarget {
+    mainSession,
+    reloadSession
+}
+
+/**
+ * Actions that a client can send to the server.
+ */
+export enum DbClientInputAction {
+    fetchData,
+    resetSession,
+    copySession,
+    disconnect,
+    getLastCudId
+}
+
+export interface InfoOption {
+    /**
+     * With the code, you can pass information about the reason of this cud operation.
+     * That can be a string (e.g. 'NewMessage') or a number (e.g. 200,304).
+     */
+    code ?: string | number;
+    /**
+     * With the data option, you can pass extra data to the
+     * cud event that gets triggered on the client.
+     */
+    data ?: any,
+}
+
+export interface TimestampOption {
+    /**
+     * With the timestamp option, you can change the sequence of data.
+     * The storage, for example, will only update data that is older as incoming data.
+     * Use this option only if you know what you are doing.
+     */
+    timestamp ?: number
+}
+
+export interface IfContainsOption {
+    /**
+     * The ifContains option gives you the possibility to define a condition
+     * that the client only inserts the value when it has data with that specific key.
+     * That can be useful if you want to reinsert old data,
+     * but only to the clients that are already loaded this old data section.
+     * Notice also that in some cases the insertion sequence is changed.
+     */
+    ifContains ?: string
+}
