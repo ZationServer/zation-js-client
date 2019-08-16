3d00a8cafbd27f82c341589480263d03c8f069da
Luca Scaringella <lucalstyle@gmail.com>
2019-08-16 17:08:44 +0200

    Updated directory structure in the test folder.

--- test/src/main/request/zationRequest.test.ts
+++ test/src/main/request/zationRequest.test.ts
@@ -4,7 +4,7 @@ GitHub: LucaCode
 Copyright(c) Luca Scaringella
  */
 
-import {ZationRequest} from "../../../../src/lib/main/request/main/zationRequest";
+import {ZationRequest}             from "../../../../src/lib/main/request/main/zationRequest";
 import {ProtocolType, RequestAble} from "../../../../src";
 
 const assert            = require("chai").assert;
