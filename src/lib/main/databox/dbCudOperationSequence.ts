968b0be2461ef2ff6b2373d5eb9c451e1510b0ee
Luca Scaringella <lucalstyle@gmail.com>
2020-01-27 02:34:27 +0100

    Update databox cud operation methods docs.

--- src/lib/main/databox/dbCudOperationSequence.ts
+++ src/lib/main/databox/dbCudOperationSequence.ts
@@ -32,30 +32,33 @@ export default class DbCudOperationSequence
      * Notice if you do a cud operation locally on the client,
      * that this operation is not done on the server-side.
      * So if the Databox reloads the data or resets the changes are lost.
+     * If you want to do more changes, you should look at the seqEdit method.
      * Insert behavior:
-     * Without ifContains (ifContains exists):
-     * Base (with selector [] or '') -> Nothing
-     * KeyArray -> Inserts the value at the end with the key
-     * (if the key does not exist). But if you are using a compare function,
-     * it will insert the value in the correct position.
-     * Object -> Inserts the value with the key (if the key does not exist).
-     * Array -> Key will be parsed to int if it is a number then it will be inserted at the index.
-     * Otherwise, it will be added at the end.
-     * With ifContains (ifContains exists):
-     * Base (with selector [] or '') -> Nothing
-     * KeyArray -> Inserts the value before the ifContains element with the key
-     * (if the key does not exist). But if you are using a compare function,
-     * it will insert the value in the correct position.
-     * Object -> Inserts the value with the key (if the key does not exist).
-     * Array -> Key will be parsed to int if it is a number then it will be inserted at the index.
-     * Otherwise, it will be added at the end.
+     * Notice that in every case, the insert only happens when the key
+     * does not exist on the client.
+     * Otherwise, the client will ignore or convert it to an
+     * update when potentiallyUpdate is active.
+     * Other conditions are that the timeout is newer than the existing
+     * timeout and all if conditions are true.
+     * Head (with selector [] or '') -> Inserts the value.
+     * KeyArray -> Inserts the value at the end with the key.
+     * But if you are using a compare function, it will insert the value in the correct position.
+     * Object -> Insert the value with the key.
+     * Array -> Key will be parsed to int if it is a number, then it will be inserted at the index.
+     * Otherwise, it will be inserted at the end.
      * @param selector
-     * The selector can be a direct key-path,
-     * can contain filter queries (by using the forint library)
-     * or it can select all items with '*'.
-     * If you use a string as a param type,
-     * you need to notice that it will be split into a path by dots.
-     * All numeric values will be converted to a string because the key can only be a string.
+     * The selector describes which key-value pairs should be
+     * deleted updated or where a value should be inserted.
+     * It can be a string array key path, but it also can contain
+     * filter queries (they work with the forint library).
+     * You can filter by value ($value or property value) by key ($key or property key) or
+     * select all keys with {} (For better readability use the constant $all).
+     * In the case of insertions, most times, the selector should end with
+     * a new key instead of a query.
+     * Notice that all numeric values in the selector will be converted to a
+     * string because all keys need to be from type string.
+     * If you provide a string instead of an array, the string will be
+     * split by dots to create a string array.
      * @param value
      * @param options
      */
@@ -69,19 +72,32 @@ export default class DbCudOperationSequence
      * Notice if you do a cud operation locally on the client,
      * that this operation is not done on the server-side.
      * So if the Databox reloads the data or resets the changes are lost.
+     * If you want to do more changes, you should look at the seqEdit method.
      * Update behavior:
-     * Base (with selector [] or '') -> Updates the complete structure.
-     * KeyArray -> Updates the specific value (if the key does exist).
-     * Object -> Updates the specific value (if the key does exist).
-     * Array -> Key will be parsed to int if it is a number it will
-     * update the specific value (if the index exist).
+     * Notice that in every case, the update only happens when the key
+     * on the client does exist.
+     * Otherwise, the client will ignore or convert it to an
+     * insert when potentiallyInsert is active.
+     * Other conditions are that the timeout is newer than the existing
+     * timeout and all if conditions are true.
+     * Head (with selector [] or '') -> Updates the complete structure.
+     * KeyArray -> Updates the specific value.
+     * Object -> Updates the specific value.
+     * Array -> Key will be parsed to int if it is a number
+     * it will update the specific value.
      * @param selector
-     * The selector can be a direct key-path,
-     * can contain filter queries (by using the forint library)
-     * or it can select all items with '*'.
-     * If you use a string as a param type,
-     * you need to notice that it will be split into a path by dots.
-     * All numeric values will be converted to a string because the key can only be a string.
+     * The selector describes which key-value pairs should be
+     * deleted updated or where a value should be inserted.
+     * It can be a string array key path, but it also can contain
+     * filter queries (they work with the forint library).
+     * You can filter by value ($value or property value) by key ($key or property key) or
+     * select all keys with {} (For better readability use the constant $all).
+     * In the case of insertions, most times, the selector should end with
+     * a new key instead of a query.
+     * Notice that all numeric values in the selector will be converted to a
+     * string because all keys need to be from type string.
+     * If you provide a string instead of an array, the string will be
+     * split by dots to create a string array.
      * @param value
      * @param options
      */
@@ -95,19 +111,32 @@ export default class DbCudOperationSequence
      * Notice if you do a cud operation locally on the client,
      * that this operation is not done on the server-side.
      * So if the Databox reloads the data or resets the changes are lost.
+     * If you want to do more changes, you should look at the seqEdit method.
      * Delete behavior:
-     * Base (with selector [] or '') -> Deletes the complete structure.
-     * KeyArray -> Deletes the specific value (if the key does exist).
-     * Object -> Deletes the specific value (if the key does exist).
-     * Array -> Key will be parsed to int if it is a number it will delete the
-     * specific value (if the index does exist). Otherwise, it will delete the last item.
+     * Notice that in every case, the delete only happens when the key
+     * on the client does exist.
+     * Otherwise, the client will ignore it.
+     * Other conditions are that the timeout is newer than the existing
+     * timeout and all if conditions are true.
+     * Head (with selector [] or '') -> Deletes the complete structure.
+     * KeyArray -> Deletes the specific value.
+     * Object -> Deletes the specific value.
+     * Array -> Key will be parsed to int if it is a number it
+     * will delete the specific value.
+     * Otherwise, it will delete the last item.
      * @param selector
-     * The selector can be a direct key-path,
-     * can contain filter queries (by using the forint library)
-     * or it can select all items with '*'.
-     * If you use a string as a param type,
-     * you need to notice that it will be split into a path by dots.
-     * All numeric values will be converted to a string because the key can only be a string.
+     * The selector describes which key-value pairs should be
+     * deleted updated or where a value should be inserted.
+     * It can be a string array key path, but it also can contain
+     * filter queries (they work with the forint library).
+     * You can filter by value ($value or property value) by key ($key or property key) or
+     * select all keys with {} (For better readability use the constant $all).
+     * In the case of insertions, most times, the selector should end with
+     * a new key instead of a query.
+     * Notice that all numeric values in the selector will be converted to a
+     * string because all keys need to be from type string.
+     * If you provide a string instead of an array, the string will be
+     * split by dots to create a string array.
      * @param options
      */
     delete(selector : DbSelector, {if : ifOption,code,data} : IfOption & InfoOption = {}) : DbCudOperationSequence {
