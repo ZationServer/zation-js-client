0ddd443a28abfd73e7e657dfd98a2bd38b1a771d
Luca Scaringella <lucalstyle@gmail.com>
2020-06-04 21:13:21 +0200

    Fix deepEqual not fully works with null.

--- src/lib/main/utils/deepEqual.ts
+++ src/lib/main/utils/deepEqual.ts
@@ -11,8 +11,8 @@ Copyright(c) Luca Scaringella
  */
 export function deepEqual(v1: any,v2: any): boolean {
     if (v1 === v2) return true;
-    if(typeof v1 === "object"){
-        if(typeof v2 === "object"){
+    if(typeof v1 === "object" && v1){
+        if(typeof v2 === "object" && v2){
             if(Array.isArray(v1) && Array.isArray(v2)){
                 for(let i = 0; i < v1.length; i++) if(!deepEqual(v1[i],v2[i])) return false;
                 return true;
