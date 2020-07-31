296caae7e930753dd0f8401be7d0b4ce5c265fec
Luca Scaringella <lucalstyle@gmail.com>
2020-07-31 14:32:40 +0200

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-SDK_VERSION=$(cat package.json | sed -n -e '/version/ s/.*: *"\([^"]*\).*/\1/p')
-echo "Building Zation Client v$SDK_VERSION...\n"
-echo "Cleaning old builds...\n"
-rm -rf zation.js zation.min.js
-echo "Zation-Client:"
-gulp browserify
-gulp minify
