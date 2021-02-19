6131044ab0e67d4d35a8facd3e9513c5edc1cc89
Luca Scaringella <lucalstyle@gmail.com>
2021-02-19 03:11:37 +0100

    Fix equal: array dif length issue.

--- src/lib/main/utils/deepEqual.ts
+++ src/lib/main/utils/deepEqual.ts
@@ -14,6 +14,7 @@ export function deepEqual(v1: any,v2: any): boolean {
     if(typeof v1 === "object" && v1){
         if(typeof v2 === "object" && v2){
             if(Array.isArray(v1) && Array.isArray(v2)){
+                if(v1.length !== v2.length) return false;
                 for(let i = 0; i < v1.length; i++) if(!deepEqual(v1[i],v2[i])) return false;
                 return true;
             }
